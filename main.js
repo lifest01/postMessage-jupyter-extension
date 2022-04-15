4f4f65a0da1b09e717a035f0c31a208f6b9e6c17
lifest01 <45783329+lifest01@users.noreply.github.com>
2022-04-15 18:44:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ define(['base/js/namespace', 'base/js/events'],
         function load_ipython_extension() {
             window.addEventListener("message", function (event) {
                 if (event.data === 'run') {
+                    console.log('run')
+                    let cells = Jupyter.notebook.get_cells();
+                    for (let i=0; i < cells.length; i++) {
+                        cells[i].execute();
+                    }
+                };
+                if (event.data === 'start') {
                     document.getElementById('header').style.display = 'none'
                     document.getElementsByClassName('end_space')[0].style.display = 'none'
                     document.getElementById("notebook-container").style.width = '1500px';
                     let cells = Jupyter.notebook.get_cells();
                     for (let i=0; i < cells.length; i++) {
                         cells[i].element.find('div.input')[0].style.display = 'none'
-                        cells[i].element.find('div.output')[0].style.height = fit-content
-                        cells[i].execute();
+                        cells[i].element.find('div.output')[0].style.height = 'fit-content'
                     }
-                }
+                };
             }, false);
         }
         return {
